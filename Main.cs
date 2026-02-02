3b198540f849881cd023f1d77c96d0515f65214f
Liz Gock <liz@blanky.dev>
2026-02-02 03:02:51 -0600

    init

---
+++ Main.cs
@@ -0,0 +1,187 @@
+﻿using MelonLoader;
+using Newtonsoft.Json;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.InputSystem;
+using UnityEngine.ResourceManagement.ResourceLocations;
+
+[assembly: MelonInfo(
+    typeof(ShrimpleMapLoader.Loader),
+    "ShrimpleMapLoader",
+    "0.0.1",
+    "ZabelTheBanal"
+)]
+
+namespace ShrimpleMapLoader;
+
+public class Loader : MelonMod
+{
+    static MapPatcher patcher = new();
+
+    public override void OnInitializeMelon()
+    {
+        LoggerInstance.Msg("haaaaai :3");
+        MelonEvents.OnSceneWasInitialized.Subscribe(patcher.OnSceneWasInitialized);
+    }
+
+    public override void OnDeinitializeMelon()
+    {
+        MelonEvents.OnSceneWasInitialized.Unsubscribe(patcher.OnSceneWasInitialized);
+    }
+
+    public override void OnSceneWasLoaded(int buildIndex, string sceneName)
+    {
+        if (sceneName is not "Splashes")
+            return;
+        LoggerInstance.Msg("Installing Extra Maps");
+        var locator = Addressables
+            .Instance.m_ResourceLocators[0]
+            .Locator.Cast<UnityEngine.AddressableAssets.ResourceLocators.ResourceLocationMap>();
+        var originalMaps = locator
+            .Locations["Maps"]
+            .Cast<Il2CppInterop.Runtime.InteropTypes.Arrays.Il2CppReferenceArray<UnityEngine.ResourceManagement.ResourceLocations.IResourceLocation>>();
+        string[] mapPaths = Directory.GetDirectories("./UserData/Maps/");
+
+        List<IResourceLocation> newMaps = new();
+        foreach (string mapPath in mapPaths)
+        {
+            if (TryLoadCustomMap(mapPath, out var info))
+            {
+                newMaps.Add(locator.Locations[info.ScenePrimaryKey][0]);
+            }
+            else
+            {
+                LoggerInstance.Warning("failed to load: " + mapPath);
+            }
+        }
+
+        Il2CppInterop.Runtime.InteropTypes.Arrays.Il2CppReferenceArray<IResourceLocation> arr =
+            new(3 + newMaps.Count);
+        arr[0] = originalMaps[0];
+        arr[1] = originalMaps[1];
+        arr[2] = originalMaps[2];
+
+        for (int i = 0; i < newMaps.Count; i++)
+        {
+            arr[3 + i] = newMaps[i];
+        }
+
+        LoggerInstance.Msg("Registering Extra Maps");
+        locator.locations.System_Collections_IDictionary_set_Item(
+            "Maps",
+            arr.Cast<Il2CppSystem.Object>()
+        );
+    }
+
+    public bool TryLoadCustomMap(string path, out MapInfo mapInfo)
+    { // messy asf code
+        string mapBasePath = Path.GetFullPath(path).Replace('\\', '/');
+        try
+        {
+            string mapJSON = File.ReadAllText($"{mapBasePath}/Manifest.json");
+            mapInfo = JsonConvert.DeserializeObject<MapInfo>(mapJSON);
+            LoggerInstance.Msg($"Installing: {mapInfo.ScenePrimaryKey}");
+            if (mapInfo.ScenePrimaryKey == null)
+                return false;
+            var locator = Addressables
+                .Instance.m_ResourceLocators[0]
+                .Locator.Cast<UnityEngine.AddressableAssets.ResourceLocators.ResourceLocationMap>();
+            if (locator.Locations.ContainsKey(mapInfo.ScenePrimaryKey))
+                return false;
+            Il2CppSystem.Collections.Generic.List<UnityEngine.ResourceManagement.ResourceLocations.IResourceLocation> depList =
+                new();
+
+            for (int i = 0; i < mapInfo.Dependencies.Length; i++)
+            {
+                var dep = mapInfo.Dependencies[i];
+                UnityEngine.ResourceManagement.ResourceProviders.AssetBundleRequestOptions req =
+                    new();
+                req.BundleName = dep.Data.BundleName;
+                req.BundleSize = dep.Data.BundleSize;
+                req.UseCrcForCachedBundle = true;
+
+                if (locator.Locations.ContainsKey(dep.PrimaryKey))
+                {
+                    var preExistingAssets = locator
+                        .Locations[dep.PrimaryKey]
+                        .Cast<Il2CppInterop.Runtime.InteropTypes.Arrays.Il2CppReferenceArray<UnityEngine.ResourceManagement.ResourceLocations.IResourceLocation>>();
+                    foreach (var asset in preExistingAssets)
+                        depList.Add(asset);
+                    continue;
+                }
+
+                UnityEngine.AddressableAssets.ResourceLocators.ContentCatalogData.CompactLocation depLocation =
+                    new(
+                        locator,
+                        $"{mapBasePath}/Dependencies/{dep.InternalId}",
+                        dep.ProviderId,
+                        null,
+                        req,
+                        0,
+                        dep.PrimaryKey,
+                        Il2CppSystem.Type.GetType(
+                            "UnityEngine.ResourceManagement.ResourceProviders.IAssetBundleResource"
+                        )
+                    );
+                depList.Add(
+                    depLocation.Cast<UnityEngine.ResourceManagement.ResourceLocations.IResourceLocation>()
+                );
+            }
+
+            UnityEngine.AddressableAssets.ResourceLocators.ContentCatalogData.CompactLocation Scenelocation =
+                new(
+                    locator,
+                    mapInfo.SceneInternalKey,
+                    "UnityEngine.ResourceManagement.ResourceProviders.BundledAssetProvider",
+                    mapInfo.ScenePrimaryKey + "_deps",
+                    null,
+                    0,
+                    mapInfo.ScenePrimaryKey,
+                    Il2CppSystem.Type.GetType(
+                        "UnityEngine.ResourceManagement.ResourceProviders.SceneInstance"
+                    )
+                );
+
+            locator.Add(
+                mapInfo.ScenePrimaryKey + "_deps",
+                depList.Cast<Il2CppSystem.Collections.Generic.IList<UnityEngine.ResourceManagement.ResourceLocations.IResourceLocation>>()
+            );
+            locator.Add(
+                mapInfo.ScenePrimaryKey,
+                Scenelocation.Cast<UnityEngine.ResourceManagement.ResourceLocations.IResourceLocation>()
+            );
+            return true;
+        }
+        catch (FileNotFoundException)
+        {
+            mapInfo = null;
+            LoggerInstance.Warning(
+                $"The map at \"{path}\" does not have a manifest.json and will not be loaded"
+            );
+            return false;
+        }
+    }
+}
+
+public struct AssetBundleRequestOptions
+{
+    public string BundleName;
+    public long BundleSize;
+    public string AssetLoadMode;
+}
+
+public struct SimpleDependency
+{
+    public string InternalId;
+    public string PrimaryKey;
+    public string ProviderId;
+
+    public AssetBundleRequestOptions Data;
+}
+
+public sealed class MapInfo
+{
+    public string SceneInternalKey;
+    public string ScenePrimaryKey;
+    public SimpleDependency[] Dependencies;
+}
---
+++ MapPatcher.cs
@@ -0,0 +1,196 @@
+using MelonLoader;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+namespace ShrimpleMapLoader;
+
+public class MapPatcher
+{
+    public bool IsTerrianSeeThroughForceEnabled = false;
+
+    public void OnSceneWasInitialized(int buildIndex, string sceneName)
+    {
+        if (IsTerrianSeeThroughForceEnabled)
+        {
+            MelonEvents.OnUpdate.Unsubscribe(stormDrain_Update);
+        }
+        if (sceneName is "Splashes" or "Bootstrap")
+            return;
+        var shaderFetch =
+            Addressables.LoadAsset<UnityEngine.ResourceManagement.ResourceProviders.IAssetBundleResource>(
+                "b75fae7cc98036167227c7451fa91add.bundle"
+            );
+        shaderFetch.WaitForCompletion();
+        Shader shader = shaderFetch
+            .Result.GetAssetBundle()
+            .LoadAsset<Shader>("Assets/Shaders/GameShaders/InvisibleWall.shader");
+        foreach (
+            MeshRenderer wall in Resources
+                .FindObjectsOfTypeAll<MeshRenderer>()
+                .Where(mesh => mesh.name.StartsWith("InvisibleWall"))
+        )
+            wall.material.shader = shader;
+
+        if (sceneName is "StormDrain")
+        {
+            IsTerrianSeeThroughForceEnabled = true;
+            MelonEvents.OnUpdate.Subscribe(stormDrain_Update);
+        }
+
+        if (sceneName is "NightRide")
+        {
+            var skyboxShaderFetch =
+                Addressables.LoadAsset<UnityEngine.ResourceManagement.ResourceProviders.IAssetBundleResource>(
+                    "dd64db8a6e3d524e4f801ea1c20e5231.bundle"
+                );
+            var manager = Il2CppView_Lighting.GlobalLightingManager.instance;
+            Material material = manager.skyboxMat;
+            manager.fog = true;
+            manager.baseLightingProfile.fogFadeEndDist = float.MaxValue;
+            manager.baseLightingProfile.UpdateShaderParams();
+
+            System.Collections.IEnumerator EnableFogRenderer()
+            {
+                while (true)
+                {
+                    yield return new WaitForEndOfFrame();
+                    if (Camera.current is Camera) // wait for a camera to be assigned (inconsistent timing on multiplayer)
+                    {
+                        foreach (Camera camera in Camera.allCameras)
+                        {
+                            if (
+                                camera.gameObject.GetComponent<Il2CppView_Lighting.FogRenderer>()
+                                is Il2CppView_Lighting.FogRenderer fogRenderer
+                            )
+                            {
+                                fogRenderer.enabled = true;
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+            MelonCoroutines.Start(EnableFogRenderer());
+
+            skyboxShaderFetch.WaitForCompletion();
+            var skyboxShader = skyboxShaderFetch
+                .Result.GetAssetBundle()
+                .LoadAsset<Shader>("Assets/Shaders/GameShaders/SkyboxShaders/CubeSkybox.shader");
+            material.shader = skyboxShader;
+        }
+
+        if (sceneName is "NightRide" or "Quarry")
+        {
+            var nightRideOSTFetch =
+                Addressables.LoadAsset<UnityEngine.ResourceManagement.ResourceProviders.IAssetBundleResource>(
+                    "1d427297405c7ef905bae9dbedf5abb2.bundle"
+                );
+            var quarryOSTFetch =
+                Addressables.LoadAsset<UnityEngine.ResourceManagement.ResourceProviders.IAssetBundleResource>(
+                    "7f48d042845aa8d546c4f67fccb2c1ac.bundle"
+                );
+            nightRideOSTFetch.WaitForCompletion();
+            quarryOSTFetch.WaitForCompletion();
+
+            AudioClip nightRideOST = nightRideOSTFetch
+                .Result.GetAssetBundle()
+                .LoadAsset<AudioClip>("Assets/Audio/Music/AFU - LB1 - NightRide.mp3");
+            AudioClip quarryOST = quarryOSTFetch
+                .Result.GetAssetBundle()
+                .LoadAsset<AudioClip>("Assets/Audio/Music/AFU - DnB6 - Quarry.mp3");
+            if (
+                UnityEngine.Resources.FindObjectsOfTypeAll<Il2CppView_Music.SongsLibrary>().First()
+                is Il2CppView_Music.SongsLibrary songLibrary
+            )
+            {
+                songLibrary.quarry = quarryOST;
+                songLibrary.nightride = nightRideOST;
+            }
+        }
+
+        //   if (sceneName is "Warehouse"){
+        //     LoggerInstance.Msg("Fixing Water");
+        //     MelonCoroutines.Start(FixWater());
+        //   }
+    }
+
+    private void stormDrain_Update()
+    {
+        try
+        {
+            Camera.current.gameObject.GetComponent<Il2CppView_Main.Camera_View>().terrainOverlapChecker.terrainOverlap = true;
+        }
+        catch (NullReferenceException)
+        {
+
+        }
+    }
+
+    internal Texture2D TexDownload(Texture gpuTex)
+    {
+        RenderTexture renderTexture = RenderTexture.GetTemporary(
+            gpuTex.width,
+            gpuTex.height,
+            0,
+            UnityEngine.SystemInfo.GetCompatibleFormat(
+                gpuTex.graphicsFormat,
+                UnityEngine.Experimental.Rendering.FormatUsage.Render
+            ),
+            1
+        );
+
+        Texture2D readableTexture = new(gpuTex.width, gpuTex.height, TextureFormat.RGBA32, false);
+
+        Graphics.Blit(gpuTex, renderTexture);
+        RenderTexture.active = renderTexture;
+        readableTexture.ReadPixels(new Rect(0, 0, gpuTex.width, gpuTex.height), 0, 0);
+
+        RenderTexture.active = null;
+        RenderTexture.ReleaseTemporary(renderTexture);
+        return readableTexture;
+    }
+
+    private System.Collections.IEnumerator FixWater()
+    {
+        var waterTextureFetch =
+            Addressables.LoadAsset<UnityEngine.ResourceManagement.ResourceProviders.IAssetBundleResource>(
+                "9ad2ca64b4d80a7f228034d0624f0c42.bundle"
+            );
+        var waterShaderFetch =
+            Addressables.LoadAsset<UnityEngine.ResourceManagement.ResourceProviders.IAssetBundleResource>(
+                "13e5358245c2906af4ba4841428bf87f.bundle"
+            );
+        var waterMaterialFetch =
+            Addressables.LoadAsset<UnityEngine.ResourceManagement.ResourceProviders.IAssetBundleResource>(
+                "aaf2c64c87f04347138362261802769c.bundle"
+            );
+        waterShaderFetch.WaitForCompletion();
+        waterShaderFetch.WaitForCompletion();
+        waterMaterialFetch.WaitForCompletion();
+
+        Texture2D waterTex = waterTextureFetch
+            .Result.GetAssetBundle()
+            .LoadAsset<Texture2D>("Assets/Materials/Terrain/Water_Caustic.png");
+        Shader waterShader = waterShaderFetch
+            .Result.GetAssetBundle()
+            .LoadAsset<Shader>("Assets/Shaders/GameShaders/Water.shader");
+        Material waterMat = waterMaterialFetch
+            .Result.GetAssetBundle()
+            .LoadAsset<Material>("Assets/Materials/Terrain/Water.mat");
+
+        yield return new WaitForEndOfFrame();
+        Texture2D tex = TexDownload(waterMat.mainTexture.Cast<Texture2D>());
+        waterMat.mainTexture = tex;
+        tex.name = "ReimportedWater";
+        tex.Apply(true, false);
+        foreach (
+            MeshRenderer renderer in Resources
+                .FindObjectsOfTypeAll<MeshRenderer>()
+                .Where(renderer => renderer.name.StartsWith("Water"))
+        )
+        {
+            renderer.material = Material.Instantiate(waterMat);
+            renderer.material.SetTexture("_MainTex", tex);
+        }
+    }
+}
